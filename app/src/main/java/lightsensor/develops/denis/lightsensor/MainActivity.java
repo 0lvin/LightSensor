9e905fb22a9d4083a5213ccff8dd6f7e5023306e
Denis Pauk <pauk.denis@gmail.com>
2015-01-25 19:50:52 +0200

    light with camra view

---
+++ app/src/main/java/lightsensor/develops/denis/lightsensor/MainActivity.java
@@ -0,0 +1,159 @@
+package lightsensor.develops.denis.lightsensor;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class MainActivity extends Activity implements SurfaceHolder.Callback, Camera.PreviewCallback, Camera.AutoFocusCallback {
+
+    private Camera camera = null;
+    private SurfaceView preview;
+    private SurfaceHolder surfaceHolder;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        for (int i = 0; i < Camera.getNumberOfCameras(); i++) {
+            CameraInfo info = new CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+                camera = Camera.open(i);
+                Camera.Parameters params = camera.getParameters();
+                params.setColorEffect(Camera.Parameters.EFFECT_MONO);
+                params.setPreviewFormat(ImageFormat.NV21);
+                camera.setParameters(params);
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (camera != null) {
+            camera.setPreviewCallback(null);
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+        camera = null;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        preview = (SurfaceView) findViewById(R.id.imageView);
+
+        surfaceHolder = preview.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        // Obtain references to the SensorManager and the Light Sensor
+        final SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_LIGHT);
+        if (sensors.size() == 0) {
+            TextView text = (TextView) findViewById(R.id.textLight);
+            text.setText("no sensors");
+        } else {
+            final Sensor lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+            // Implement a listener to receive updates
+            SensorEventListener listener = new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent event) {
+                    float lightQuantity = event.values[0];
+                    TextView text = (TextView) findViewById(R.id.textLight);
+                    text.setText(Float.toString(lightQuantity));
+                }
+
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                }
+            };
+
+            // Register the listener with the light sensor -- choosing
+            // one of the SensorManager.SENSOR_DELAY_* constants.
+            sensorManager.registerListener(
+                    listener, lightSensor, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    @Override
+    public void onAutoFocus(boolean success, Camera camera) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.setPreviewCallback(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Camera.Size previewSize = camera.getParameters().getPreviewSize();
+        float aspect = (float) previewSize.width / previewSize.height;
+
+        int previewSurfaceWidth = preview.getWidth();
+        int previewSurfaceHeight = preview.getHeight();
+
+        LayoutParams lp = preview.getLayoutParams();
+
+        // здесь корректируем размер отображаемого preview, чтобы не было искажений
+
+        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+            // портретный вид
+            camera.setDisplayOrientation(90);
+            lp.height = previewSurfaceHeight;
+            lp.width = (int) (previewSurfaceHeight / aspect);
+
+        } else {
+            // ландшафтный
+            camera.setDisplayOrientation(0);
+            lp.width = previewSurfaceWidth;
+            lp.height = (int) (previewSurfaceWidth / aspect);
+        }
+
+        preview.setLayoutParams(lp);
+        camera.startPreview();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        Camera.Parameters params = camera.getParameters();
+        int exposition = params.getExposureCompensation();
+        TextView text = (TextView) findViewById(R.id.textCameraLight);
+        text.setText(Integer.toString(exposition));
+    }
+}
