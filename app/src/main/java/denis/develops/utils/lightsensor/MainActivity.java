36037b3e4d18737f52b8a2bcb38a58038652e0ba
Denis Pauk <pauk.denis@gmail.com>
2015-01-31 16:29:08 +0200

    code cleanups

--- app/src/main/java/denis/develops/utils/lightsensor/MainActivity.java
+++ app/src/main/java/denis/develops/utils/lightsensor/MainActivity.java
@@ -30,6 +30,7 @@ import java.util.List;
 
 public class MainActivity extends Activity implements SurfaceHolder.Callback, Camera.PreviewCallback, Camera.AutoFocusCallback {
 
+    final String MAGNITUDE_VALUE = "MagnitudeValue";
     private ContentResolver cResolver;
     private int brightness = 0;
     private int magnitude = 10;
@@ -38,6 +39,11 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
     private Camera camera = null;
     private SurfaceView preview;
     private SurfaceHolder surfaceHolder;
+    private TextView textAuthor;
+    private TextView textLightSensor;
+    private boolean usedFront = false;
+    private TextView textCameraLight;
+    private TextView textMagnitude;
 
     public static float getMiddleIntense(byte[] data, int width, int height) {
         long sum = 0;
@@ -48,25 +54,67 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
         return sum / size;
     }
 
+    private Camera.Size getMinimalPreviewSize(Camera.Parameters params) {
+        int minWidth = -1;
+        List<Camera.Size> sizes = params.getSupportedPreviewSizes();
+        for (int i = 0; i < sizes.size(); i++) {
+            int compareWidth = sizes.get(i).width;
+            if (minWidth == -1) {
+                minWidth = compareWidth;
+            } else if (minWidth > compareWidth) {
+                minWidth = compareWidth;
+            }
+        }
+        for (int i = 0; i < sizes.size(); i++) {
+            if (sizes.get(i).width == minWidth) {
+                return sizes.get(i);
+            }
+        }
+        //something wrong, but try return first
+        return sizes.get(0);
+    }
+
+    /*
+        returned camera, front camera proffered
+     */
+    private int getCameraId() {
+        CameraInfo info = new CameraInfo();
+        //search front camera
+        for (int i = 0; i < Camera.getNumberOfCameras(); i++) {
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+                usedFront = true;
+                return i;
+            }
+        }
+        // looks as front not exist
+        for (int i = 0; i < Camera.getNumberOfCameras(); i++) {
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_BACK) {
+                usedFront = false;
+                return i;
+            }
+        }
+        return -1;
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
-        for (int i = 0; i < Camera.getNumberOfCameras(); i++) {
-            CameraInfo info = new CameraInfo();
-            Camera.getCameraInfo(i, info);
-            if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
-                camera = Camera.open(i);
-                Camera.Parameters params = camera.getParameters();
-                params.setColorEffect(Camera.Parameters.EFFECT_MONO);
-                params.setPreviewFormat(ImageFormat.NV21);
-                if (params.isAutoExposureLockSupported()) {
-                    params.setAutoExposureLock(true);
-                    params.setExposureCompensation(1);
-                }
 
-                camera.setParameters(params);
-                return;
+        int cameraId = this.getCameraId();
+        if (cameraId != -1) {
+            camera = Camera.open(cameraId);
+            Camera.Parameters params = camera.getParameters();
+            params.setColorEffect(Camera.Parameters.EFFECT_MONO);
+            params.setPreviewFormat(ImageFormat.NV21);
+            if (params.isAutoExposureLockSupported()) {
+                params.setAutoExposureLock(true);
+                params.setExposureCompensation(1);
             }
+            Camera.Size previewSize = getMinimalPreviewSize(params);
+            params.setPreviewSize(previewSize.width, previewSize.height);
+            camera.setParameters(params);
         }
     }
 
@@ -82,42 +130,12 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
         camera = null;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        TextView author = (TextView) findViewById(R.id.textViewAuthor);
-
-        author.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://0lvin.blogspot.com/"));
-                startActivity(browserIntent);
-            }
-        });
-
-        //Get the content resolver
-        cResolver = getContentResolver();
-
-        bar = (ProgressBar) findViewById(R.id.brightnessValue);
-        magnitude_seek = (SeekBar) findViewById(R.id.magnitudeValue);
-        magnitude_seek.setMax(40);
-        magnitude_seek.setProgress(magnitude);
-
-        bar.setMax(256);
-        preview = (SurfaceView) findViewById(R.id.imageView);
-
-        surfaceHolder = preview.getHolder();
-        surfaceHolder.addCallback(this);
-        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
+    private void initLightSensor() {
         // Obtain references to the SensorManager and the Light Sensor
         final SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_LIGHT);
         if (sensors.size() == 0) {
-            TextView text = (TextView) findViewById(R.id.textLight);
-            text.setText("Light by sensor: no sensors");
+            textLightSensor.setText("Light by sensor: no sensors");
         } else {
             final Sensor lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
@@ -126,8 +144,7 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
                 @Override
                 public void onSensorChanged(SensorEvent event) {
                     int lightQuantity = (int) event.values[0];
-                    TextView text = (TextView) findViewById(R.id.textLight);
-                    text.setText("Light by sensor: " + Integer.toString(lightQuantity));
+                    textLightSensor.setText("Light by sensor: " + Integer.toString(lightQuantity));
                 }
 
                 @Override
@@ -136,21 +153,70 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
                 }
             };
 
-            // Register the listener with the light sensor -- choosing
-            // one of the SensorManager.SENSOR_DELAY_* constants.
             sensorManager.registerListener(
                     listener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-            try {
-                //Get the current system brightness
-                brightness = System.getInt(cResolver, System.SCREEN_BRIGHTNESS);
-            } catch (SettingNotFoundException e) {
-                //Throw an error case it couldn't be retrieved
-                Log.e("Error", "Cannot access system brightness");
-                e.printStackTrace();
-            }
-            bar.setProgress(brightness);
         }
+    }
+
+    private void easterEggInit() {
+        textAuthor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://0lvin.blogspot.com/"));
+                startActivity(browserIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textMagnitude = (TextView) findViewById(R.id.textMagnitude);
+
+        textAuthor = (TextView) findViewById(R.id.textViewAuthor);
+        textLightSensor = (TextView) findViewById(R.id.textLight);
+        textCameraLight = (TextView) findViewById(R.id.textCameraLight);
+        bar = (ProgressBar) findViewById(R.id.brightnessValue);
+        magnitude_seek = (SeekBar) findViewById(R.id.magnitudeValue);
+        preview = (SurfaceView) findViewById(R.id.imageView);
+        cResolver = getContentResolver();
+
+        this.initLightSensor();
+        this.updateBrightnessBar();
+        this.easterEggInit();
+
+        if (savedInstanceState != null) {
+            magnitude = savedInstanceState.getInt(this.MAGNITUDE_VALUE);
+        }
+
+        magnitude_seek.setMax(40);
+        magnitude_seek.setProgress(magnitude);
+
+        bar.setMax(256);
+        bar.setProgress(this.brightness);
+
+        surfaceHolder = preview.getHolder();
+        surfaceHolder.addCallback(this);
+    }
+
+    private void updateBrightnessBar() {
+        try {
+            //Get the current system brightness
+            brightness = System.getInt(cResolver, System.SCREEN_BRIGHTNESS);
+        } catch (SettingNotFoundException e) {
+            //Throw an error case it couldn't be retrieved
+            Log.e("Error", "Cannot access system brightness");
+            e.printStackTrace();
+        }
+        bar.setProgress(brightness);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(this.MAGNITUDE_VALUE, this.magnitude);
     }
 
     @Override
@@ -175,7 +241,6 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
                 LayoutParams lp = preview.getLayoutParams();
 
                 // fix orientation
-
                 if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
                     // portrait
                     camera.setDisplayOrientation(90);
@@ -212,21 +277,12 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
         Camera.Parameters params = camera.getParameters();
         Camera.Size size = params.getPreviewSize();
         float intense = this.getMiddleIntense(data, size.width, size.height);
-        TextView text = (TextView) findViewById(R.id.textCameraLight);
-        text.setText("Light by camera: " + Integer.toString((int) (SensorManager.LIGHT_SHADE * intense / 255)));
-        try {
-            //Get the current system brightness
-            brightness = System.getInt(cResolver, System.SCREEN_BRIGHTNESS);
-        } catch (SettingNotFoundException e) {
-            //Throw an error case it couldn't be retrieved
-            Log.e("Error", "Cannot access system brightness");
-            e.printStackTrace();
-        }
+        this.updateBrightnessBar();
+        textCameraLight.setText("Light by camera: " + Integer.toString((int) (SensorManager.LIGHT_OVERCAST * intense / 255)));
         magnitude = magnitude_seek.getProgress();
         float magnitude_x = (((float) magnitude + 10) / 20);
         float new_intense = intense * magnitude_x;
 
-        TextView textMagnitude = (TextView) findViewById(R.id.textMagnitude);
         textMagnitude.setText(Float.toString(magnitude_x) + "x");
         if (new_intense > 255) {
             new_intense = 255;
