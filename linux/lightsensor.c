e7f05e7e777f1fb21e3b83af1c5e59b5791b6ebd
Denis Pauk <pauk.denis@gmail.com>
2015-10-24 00:12:43 +0300

    add update code

--- linux/lightsensor.c
+++ linux/lightsensor.c
@@ -27,11 +27,23 @@ int get_status(int dev_fd) {
 	return flags;
 }
 
-void get_abs_value(int dev_fd) {
+double get_abs_value(int dev_fd) {
 	struct input_absinfo absinfo;
 	int err;
+	int predefined_lux[10] = {
+		10, 160, 225, 320, 640,
+		1280, 2600, 5800, 8000, 10240
+	};
+	double lux = 0;
 	err = ioctl(dev_fd, EVIOCGABS(EVENT_TYPE_LIGHT), &absinfo);
-	printf("Value %d => %d\n", err, absinfo.value);
+	if (absinfo.value >= 10) {
+		lux = (double)predefined_lux[9] / 10.0 * absinfo.value;
+	} else {
+		lux = predefined_lux[absinfo.value];
+	}
+
+	printf("Value %d => %d, %.1f lux\n", err, absinfo.value, lux);
+	return lux;
 }
 
 #define min_value(a, b) ((a) < (b)) ? (a) : (b)
@@ -125,14 +137,69 @@ char * get_device(const char * dev_name) {
 	return NULL;
 }
 
+int update_backlight(char* control, double value, int offset) {
+	// try to open contol and update value
+	int changed = 0;
+	char file_name_buffer[256] = {0};
+	sprintf(file_name_buffer, "/sys/class/%s/max_brightness", control);
+	int max_file = open(file_name_buffer, O_RDONLY);
+	sprintf(file_name_buffer, "/sys/class/%s/brightness", control);
+	int value_file = open(file_name_buffer, O_WRONLY);
+	if (max_file > 0 && value_file > 0) {
+		char max_value_char[6] = {0};
+		char value_char[6] = {0};
+		if (read(max_file, max_value_char, 5)) {
+			int max_value = strtol(max_value_char, NULL, 10);
+			int for_set = max_value * value / 10240 + offset;
+			if (for_set >= max_value) {
+				for_set = max_value - 1;
+			}
+			sprintf(value_char, "%d", for_set);
+			write(value_file, value_char, strlen(value_char));
+			printf("Update '%s' to %s/%d\n", file_name_buffer, value_char, max_value);
+			changed = 1;
+		}
+	} else {
+		printf("Can't open backlight control '%s'\n", file_name_buffer);
+	}
+	close(max_file);
+	close(value_file);
+	return changed;
+}
+
+void update_lights(double value) {
+	// update value for all known contols
+	char backlight_dirs[][50] = {
+		"backlight/acpi_video0",
+		"leds/lcd-backlight"
+	};
+	int i, changed = 0;
+	// update by virtual values like acpi
+	for (i = 0; i < 2; i++) {
+		if (update_backlight(backlight_dirs[i], value, 0))
+			changed = 1;
+	}
+	// we can't change virtual control, try update by direct contol
+	if (!changed) {
+		char backlight_direct_dirs[][50] = {
+			"backlight/radeon_bl0",
+		};
+		for (i = 0; i < 1; i++) {
+			update_backlight(backlight_direct_dirs[i], value, 1);
+		}
+	}
+}
+
 int main() {
 	int dev_fd = open(DEVICE_NAME, O_RDONLY);
 	if (dev_fd >= 0) {
 		if (!get_status(dev_fd))
 			enable_sensor(dev_fd, 1);
 	} else {
-		printf("Light sensor swith have not found.\n");
+		printf("Light sensor swith has not found.\n");
 	}
+	double sum_light = 0;
+	int count_light = 0;
 	char * device = get_device("lightsensor-level");
 	if (device) {
 		printf("devices: %s\n", device);
@@ -148,20 +215,20 @@ int main() {
 			int input_fd = open(one_dev, O_RDONLY);
 			if (input_fd >= 0) {
 				printf("Try use:'%s'\n", one_dev);
-				get_abs_value(input_fd);
+				sum_light += get_abs_value(input_fd);
+				count_light ++;
 			} else {
 				printf("Cant access to '%s'\n", one_dev);
 			}
 		}
 		free(device);
 	} else {
-		printf("Light sensors have not found.\n");
+		printf("Light sensor has not found.\n");
 	}
-	// change backlight
-	// echo 10 > /sys/class/backlight/radeon_bl0/brightness
-	// echo 10 > /sys/class/backlight/acpi_video0/brightness
-	// screen brightness
-	// /sys/class/leds/lcd-backlight/brightness
+	if (count_light) {
+		sum_light = sum_light / count_light;
+	}
+	update_lights(sum_light);
 	// flashlight
 	// /sys/class/leds/flashlight/brightness
 	// 0 - none
